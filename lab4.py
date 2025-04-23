a37c8fd09688603546a0e856c036d737b26b1ac5
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2025-04-23 14:35:15 +0300

    Fix

--- lab4.py
+++ lab4.py
@@ -9,48 +9,73 @@ a, b = 0.0, 1.0
 eps = 0.001
 y0 = 1.0
 dy0 = 5.0
-n = 100
-h = (b-a) / n
+h = 0.5
+precise_order = (1 << 4 - 1)
 
-
-def dydx(x, y1, y2):
-    dy1 = y2
-    dy2 = 8 * y2 - 7 * y1 + 14
-    return dy1, dy2
-
-def runge_kutta_step(x, y1, y2, h):
-    k1y1, k1y2 = dydx(x, y1, y2)
-    k2y1, k2y2 = dydx(x + h/2, y1 + h/2 * k1y1, y2 + h/2 * k1y2)
-    k3y1, k3y2 = dydx(x + h/2, y1 + h/2 * k2y1, y2 + h/2 * k2y2)
-    k4y1, k4y2 = dydx(x + h, y1 + h * k3y1, y2 + h * k3y2)
-
-    next_y1 = y1 + h * (k1y1 + 2*k2y1 + 2*k3y1 + k4y1) / 6
-    next_y2 = y2 + h * (k1y2 + 2*k2y2 + 2*k3y2 + k4y2) / 6
-    return next_y1, next_y2
 
 def exact_solution(x):
     C1 = 1
     C2 = -2
     return C1 * math.exp(7*x) + C2 * math.exp(x) + 2
 
-x_values = list()
-y_num = list()
-dy_num = list()
-y_exact = list()
 
-x = a
-y1 = y0
-y2 = dy0
+def dydx(y1, y2):
+    return y2, 8 * y2 - 7 * y1 + 14
 
-while x <= b:
-    x_values.append(x)
-    y_num.append(y1)
-    y_exact.append(exact_solution(x))
-    dy_num.append(y2)
 
-    y1, y2 = runge_kutta_step(x, y1, y2, h)
-    x += h
+def runge_kutta_step(y1, y2, h):
+    k1y1, k1y2 = dydx(y1, y2)
+    k2y1, k2y2 = dydx(y1 + h / 2 * k1y1, y2 + h / 2 * k1y2)
+    k3y1, k3y2 = dydx(y1 + h / 2 * k2y1, y2 + h / 2 * k2y2)
+    k4y1, k4y2 = dydx(y1 + h * k3y1, y2 + h * k3y2)
 
-print('|       x         |       y\'      |       y\'\'     |       y\'точное |       delta    |')
-for i in range(len(x_values)):
-    print('|{:14.7f}   |{:14.7f} |{:14.7f} |{:14.7f}  |{:14.7f}  |'.format(x_values[i], y_num[i], dy_num[i], y_exact[i], abs(y_exact[i] - y_num[i])))
+    ny = y1 + h * (k1y1 + 2 * k2y1 + 2 * k3y1 + k4y1) / 6
+    ny2 = y2 + h * (k1y2 + 2 * k2y2 + 2 * k3y2 + k4y2) / 6
+
+    return ny, ny2
+
+results = []
+
+for i in range(1000):
+    max_error = -1
+    x = a
+    y1 = y0
+    y2 = dy0
+    current_results = []
+
+    exact = exact_solution(x)
+    current_results.append((x, y1, 0.0))
+
+    while x < b:
+        y1_h, y2_h = runge_kutta_step(y1, y2, h)
+
+        y1_mid, y2_mid = runge_kutta_step(y1, y2, h/2)
+        y1_2h, y2_2h = runge_kutta_step(y1_mid, y2_mid, h/2)
+
+        error = abs(y1_h - y1_2h) / precise_order
+        x_new = x + h
+
+        if x_new > b:
+            break
+
+        current_results.append((x_new, y1_2h, error))
+        max_error = max(max_error, error)
+
+        y1, y2 = y1_2h, y2_2h
+        x = x_new
+
+    results.append((h, current_results))
+
+    if max_error < eps:
+        print('|       x         |       y\'      |       error   |       y\'точное |       delta    |')
+        for entry in current_results:
+            x_val, approx, err = entry
+
+            ex = exact_solution(x_val)
+
+            print('|{:14.7f}   |{:14.7f} |{:14.7f} |{:14.7f}  |{:14.7f}  |'.format(x_val, approx, err, ex, abs(approx - ex)))
+        print(f'ε = {eps}, h = {h}')
+        break
+
+    h /= 2
+
